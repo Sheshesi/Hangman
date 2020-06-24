210bc216f18fef903f7f306f8c35f0ae5962e877
GrooEgor <milanov_i@mail.ru>
2020-06-25 02:10:46 +0700

    Comleted logo

--- src/logo.cpp
+++ src/logo.cpp
@@ -4,12 +4,57 @@
 using namespace sf;
 void Logo::ShowLogo(RenderWindow& window)
 {
+    Music music;
+    Image titleImage;
+    Texture titleTexture;
+    Sprite titleSprite;
+    Clock clock;
+    music.openFromFile("audio/Logo.ogg");
+    music.play();
+    titleImage.loadFromFile("images/TITLE.png");
+    titleTexture.loadFromImage(titleImage);
+    titleTexture.setSmooth(true);
+    titleSprite.setTexture(titleTexture);
+    titleSprite.setColor(Color::White);
+    FloatRect fRect = titleSprite.getLocalBounds();
+    titleSprite.setOrigin(
+            fRect.left + fRect.width / 2.0f,
+            fRect.top + fRect.height / 2.0f); //Определение центра спрайта
+    titleSprite.setPosition(
+            window.getSize().x / 2,
+            window.getSize().y / 2); //Выставление спрайта по центру экрана
+    float scale = 0.8; //Изначальный масштаб для спрайта
+    float fadeBegin = 0; //Изначальная прозрачность спрайта
+    float fadeEnd = 240; //Конечная прозрачность спрайта
     bool isShowLogo = true;
     Event event;
     while (isShowLogo) {
         while (window.pollEvent(event)) {
             if (Keyboard::isKeyPressed(Keyboard::Escape)
                 || Keyboard::isKeyPressed(Keyboard::Space)) {
+                isShowLogo = false;
+            }
+        }
+        float time
+                = clock.getElapsedTime().asMicroseconds(); //Определение времени
+                                                           //как микросекунды
+        clock.restart();    //Перезапуск времени
+        time = time / 4500; //Скорость игры
+        window.clear();     //Очистка экрана
+        if (fadeBegin < 240) {
+            fadeBegin += 0.3 * time;
+            titleSprite.setColor(Color(255, 255, 255, fadeBegin));
+            scale += 0.0001 * time;
+            titleSprite.setScale(scale, scale);
+            window.draw(titleSprite);
+            window.display();
+        } else {
+            if (fadeEnd > 0) {
+                fadeEnd -= 0.5 * time;
+                titleSprite.setColor(Color(255, 255, 255, fadeEnd));
+                window.draw(titleSprite);
+                window.display();
+            } else {
                 isShowLogo = false;
             }
         }
