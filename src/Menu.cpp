800115c89b97a9ecc652a9f2f9831ff3758360cb
Sheshesi <alexkadyrbayev@gmail.com>
2020-06-26 01:00:57 +0700

    Added sound in Menu.cpp

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,6 +5,10 @@
 
 using namespace sf;
 void Menu::menu(RenderWindow &window, Music &jazz) {
+  SoundBuffer buffer;
+  buffer.loadFromFile("audio/Click.ogg");
+  Sound sound;
+  sound.setBuffer(buffer);
   bool isMenu = true;
   while (isMenu) {
     sf::Event event;
