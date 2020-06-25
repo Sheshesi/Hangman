7a04d3328e5f53fd1d5ba4c7da0dd3406405ee8a
Sheshesi <alexkadyrbayev@gmail.com>
2020-06-26 01:18:14 +0700

    Update Menu.cpp

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -5,10 +5,18 @@
 
 using namespace sf;
 void Menu::menu(RenderWindow &window, Music &jazz) {
+  Font font;
+  Clock clock;
   SoundBuffer buffer;
   buffer.loadFromFile("audio/Click.ogg");
   Sound sound;
   sound.setBuffer(buffer);
+  font.loadFromFile("19183.ttf");
+  Text text("Author: Shegross\n Version(0.1)", font);
+  text.setCharacterSize(30);
+  text.setStyle(sf::Text::Regular);
+  text.setFillColor(Color::White);
+  text.setPosition(1100, 680);
   bool isMenu = true;
   while (isMenu) {
     sf::Event event;
