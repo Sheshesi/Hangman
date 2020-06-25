e85c726c3f59695e6adc54051dae5b1477e64b6d
Sheshesi <alexkadyrbayev@gmail.com>
2020-06-26 02:04:59 +0700

    Initialization of textures

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -17,7 +17,30 @@ void Menu::menu(RenderWindow &window, Music &jazz) {
   text.setStyle(sf::Text::Regular);
   text.setFillColor(Color::White);
   text.setPosition(1100, 680);
+  Texture newGame, howToPlay, Quit, aboutMenu, titleTexture, hangTexture,
+      manTexture, shadowTexture, options;
+  manTexture.loadFromFile("images/hang/man.png");
+  shadowTexture.loadFromFile("images/menu/Shadow.png");
+  newGame.loadFromFile("images/menu/NewGame.png");
+  howToPlay.loadFromFile("images/menu/HowToPlay.png");
+  Quit.loadFromFile("images/menu/Quit.png");
+  titleTexture.loadFromFile("images/Title.png");
+  hangTexture.loadFromFile("images/menu/Hang.png");
+  options.loadFromFile("images/menu/Options.png");
+  options.setSmooth(true);
+  hangTexture.setSmooth(true);
+  newGame.setSmooth(true);
+  howToPlay.setSmooth(true);
+  Quit.setSmooth(true);
+  manTexture.setSmooth(true);
+  shadowTexture.setSmooth(true);
+  Sprite ButtonNG(newGame), ButtonHTP(howToPlay), ButtonQ(Quit),
+      ButtonO(options), TitleSprite(titleTexture), HangSprite(hangTexture),
+      ManSprite(manTexture), ShadowSprite(shadowTexture);
+  Sprite shadowButtonNG(newGame), shadowButtonHTP(howToPlay),
+      shadowButtonQ(Quit), shadowButtonO(options);
   bool isMenu = true;
+  int menuNum = 0;
   while (isMenu) {
     sf::Event event;
     while (window.pollEvent(event)) {
