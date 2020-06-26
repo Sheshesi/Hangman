6ad0dbe1d139fd12c78420510db415ee69093d8f
Sheshesi <alexkadyrbayev@gmail.com>
2020-06-26 19:49:56 +0700

    Call menu function in main

--- src/Hangman.cpp
+++ src/Hangman.cpp
@@ -1,11 +1,19 @@
 #include "SFML/Audio.hpp"
 #include "SFML/Graphics.hpp"
 #include "logo.h"
+#include "Menu.h"
 using namespace sf;
 int main()
 {
     RenderWindow window(VideoMode(1366, 768), "Hangman", Style::Fullscreen);
+    Font font;
     Logo logo;
+    Music jazz;
+	Sound sound;
+	Sound clap;
+    Menu menuShow;
+	SoundBuffer buffer, buffer2;
     logo.ShowLogo(window);
+    menuShow.menu(window,jazz);
     return 0;
 }
