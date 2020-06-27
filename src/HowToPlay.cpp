2bce343c1433fe539e7d63735b6013f0b6f0e5e2
GrooEgor <milanov_i@mail.ru>
2020-06-27 23:29:18 +0700

    Initializing sprites in HowToPlay.cpp

--- src/HowToPlay.cpp
+++ src/HowToPlay.cpp
@@ -10,4 +10,9 @@ void howToButton::HowToPlay(RenderWindow& window)
     backButtonShadow.loadFromFile("images/menu/Back.png");
     backButtonShadow.setSmooth(true);
     backButton.setSmooth(true);
+    Sprite buttonBack(backButton), buttonBackShadow(backButtonShadow);
+    buttonBackShadow.setScale(0.6, 0.6);
+    buttonBackShadow.setColor(Color(0, 0, 0, 64));
+    buttonBackShadow.setPosition(560, 550);
+    buttonBack.setScale(0.6, 0.6);
 }
