b275689aff903de5bfb26ffa7304df1121621f8f
Sheshesi <alexkadyrbayev@gmail.com>
2020-06-28 22:56:10 +0700

    Changed names of some variables

--- src/Hangman.cpp
+++ src/Hangman.cpp
@@ -23,41 +23,41 @@ struct themesChoise{
   string themeNine = "Animals";
 }themeNumber;
 
-string choiseTheTheme(int themes, string* path) {
-	themes = rand() % 9 + 1;
-	if (themes == 1)
+string choiseTheTheme(int num, string* path) {
+	num = rand() % 9 + 1;
+	if (num == 1)
 	{
 		*path = themeNumber.themeOne;
 	}
-	if (themes == 2)
+	if (num == 2)
 	{
 		*path = themeNumber.themeTwo;
 	}
-	if (themes == 3)
+	if (num == 3)
 	{
 		*path = themeNumber.themeThree;
 	}
-	if (themes == 4)
+	if (num == 4)
 	{
 		*path = themeNumber.themeFour;
 	}
-	if (themes == 5)
+	if (num == 5)
 	{
 		*path = themeNumber.themeFive;
 	}
-	if (themes == 6)
+	if (num == 6)
 	{
 		*path = themeNumber.themeSix;
 	}
-	if (themes == 7)
+	if (num == 7)
 	{
 		*path = themeNumber.themeSeven;
 	}
-	if (themes == 8)
+	if (num == 8)
 	{
 		*path = themeNumber.themeEight;
 	}
-	if (themes == 9)
+	if (num == 9)
 	{
 		*path = themeNumber.themeNine;
 	}
@@ -67,8 +67,6 @@ string choiseTheTheme(int themes, string* path) {
 void loadFromFile(vector<Sprite> &sprt, vector<Image> &img, vector<Texture> &txtr, vector<Sprite> &shadow) {
 	vector<string> symbol;
 	char letter = 'A';
-	int x = 600;
-	int y = 100;
 	string word[26];
 	string way = "images/alphabet/";
 	for (int i = 0; i < MAX_SIZE; i++) {
@@ -87,9 +85,10 @@ void loadFromFile(vector<Sprite> &sprt, vector<Image> &img, vector<Texture> &txt
 		shadow[i].setColor(Color(0, 0, 0, 64));
 	}
 }
-
+ 
 int main() {
   RenderWindow window(VideoMode(1366, 768), "Hangman");
+  srand((unsigned)time(NULL));
   Font font;
   Logo logo;
   Music jazz;
@@ -97,10 +96,13 @@ int main() {
   Sound clap;
   Menu menuShow;
   SoundBuffer buffer, buffer2;
+  string path;
+  int num;
   jazz.openFromFile("audio/Jazz.ogg");
   logo.ShowLogo(window);
   jazz.play();
   jazz.setLoop(true);
   menuShow.menu(window, jazz);
+  choiseTheTheme(num,&path);
   return 0;
 }
