203d4dfe105c3083b6d124711a1b6f9539f41484
Sheshesi <alexkadyrbayev@gmail.com>
2020-06-28 23:30:23 +0700

    Added logic function

---
+++ src/logic.cpp
@@ -0,0 +1,8 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "logic.hpp"
+using namespace sf;
+using namespace std;
+void mainLogic::logicFunction(RenderWindow& window,vector<Sprite> &sprt, vector<Texture> &txtr, vector<Sprite> &shadow){
+
+}
