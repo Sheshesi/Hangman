87a72f0aebaee1f8d1d8fa162ca18bd7f0ebdac9
GrooEgor <milanov_i@mail.ru>
2020-06-28 16:54:43 +0700

    Formatted Hangman.cpp with clang-format

--- src/Hangman.cpp
+++ src/Hangman.cpp
@@ -1,22 +1,21 @@
+#include "Menu.h"
 #include "SFML/Audio.hpp"
 #include "SFML/Graphics.hpp"
 #include "logo.h"
-#include "Menu.h"
 using namespace sf;
-int main()
-{
-    RenderWindow window(VideoMode(1366, 768), "Hangman");
-    Font font;
-    Logo logo;
-    Music jazz;
-	Sound sound;
-	Sound clap;
-    Menu menuShow;
-	SoundBuffer buffer, buffer2;
-    jazz.openFromFile("audio/Jazz.ogg");
-    logo.ShowLogo(window);
-    jazz.play();
-    jazz.setLoop(true);
-    menuShow.menu(window,jazz);
-    return 0;
+int main() {
+  RenderWindow window(VideoMode(1366, 768), "Hangman");
+  Font font;
+  Logo logo;
+  Music jazz;
+  Sound sound;
+  Sound clap;
+  Menu menuShow;
+  SoundBuffer buffer, buffer2;
+  jazz.openFromFile("audio/Jazz.ogg");
+  logo.ShowLogo(window);
+  jazz.play();
+  jazz.setLoop(true);
+  menuShow.menu(window, jazz);
+  return 0;
 }
