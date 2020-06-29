e0c6e60399a1f5cdb47c325a36efae774353d009
GrooEgor <milanov_i@mail.ru>
2020-06-29 16:32:23 +0700

    Initializing textures in Lose.cpp

--- src/Lose.cpp
+++ src/Lose.cpp
@@ -9,4 +9,8 @@ bool loseWindow::Lose(RenderWindow& window)
 	wind.loadFromFile("images/Lose/1x/window.png");
 	yes.loadFromFile("images/Lose/1x/Yes.png");
 	no.loadFromFile("images/Lose/1x/No.png");
+    Texture Wind, Yes, No;
+	Wind.loadFromImage(wind);
+	Yes.loadFromImage(yes);
+	No.loadFromImage(no);
 }
