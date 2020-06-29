adad439106d0216cfc9e55a62ff8f15e3eedde1d
GrooEgor <milanov_i@mail.ru>
2020-06-29 15:10:44 +0700

    Initializing images in Win.cpp

---
+++ src/Win.cpp
@@ -0,0 +1,11 @@
+#include "Win.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+using namespace sf;
+using namespace std;
+bool winWindow::Win(RenderWindow& window) {
+    Image wind, yes, no;
+	wind.loadFromFile("images/Win/1x/window.png");
+	yes.loadFromFile("images/Win/1x/yes.png");
+	no.loadFromFile("images/Win/1x/no.png");
+}
