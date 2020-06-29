3007ed7d46f02c1d75c0c6bfbf715db3b170b171
GrooEgor <milanov_i@mail.ru>
2020-06-29 16:30:50 +0700

    Initializing images in Lose.cpp

---
+++ src/Lose.cpp
@@ -0,0 +1,12 @@
+#include "Lose.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+using namespace sf;
+using namespace std;
+bool loseWindow::Lose(RenderWindow& window)
+{
+	Image wind, yes, no;
+	wind.loadFromFile("images/Lose/1x/window.png");
+	yes.loadFromFile("images/Lose/1x/Yes.png");
+	no.loadFromFile("images/Lose/1x/No.png");
+}
