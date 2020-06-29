dbbb13c21045ef781dd4d7a20cb8403147dc28ea
Sheshesi <alexkadyrbayev@gmail.com>
2020-06-29 21:11:36 +0700

    Changed way of SFML

--- test/logo_test.cpp
+++ test/logo_test.cpp
@@ -1,21 +1,25 @@
 #define CATCH_CONFIG_MAIN
-#include <catch.hpp>
-#include <logo.h>
 #include "SFML/Graphics.hpp"
+#include "logo.hpp"
+#include <catch.hpp>
 using namespace sf;
-TEST_CASE("logo_test","logo"){
-    GIVEN("getPosition"){
-        RenderWindow window(VideoMode(1366,768),"Hangman");
+TEST_CASE("logo_test", "logo")
+{
+    GIVEN("getPosition")
+    {
+        RenderWindow window(VideoMode(1366, 768), "Hangman");
         Texture titleTexture, testTexture;
         Sprite titleSprite, testSprite;
         titleTexture.loadFromFile("images/TITLE.png");
         testTexture.loadFromFile("images/TITLE.png");
         titleSprite.setTexture(titleTexture);
         testSprite.setTexture(testTexture);
-        WHEN("Using logo function"){
+        WHEN("Using logo function")
+        {
             Logo logo;
             logo.ShowLogo(window);
-            THEN("Changed logo position"){
+            THEN("Changed logo position")
+            {
                 REQUIRE(titleSprite.getScale() == testSprite.getScale());
             }
         }
