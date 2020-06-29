181a920ed0da4133c6904a7860582be67ccdeb29
Sheshesi <alexkadyrbayev@gmail.com>
2020-06-29 13:49:24 +0700

    Added arguments in logic

--- src/logic.hpp
+++ src/logic.hpp
@@ -5,5 +5,5 @@ using namespace sf;
 using namespace std;
 class mainLogic{
 public:
-    void logicFunction(RenderWindow& window,vector<Sprite> &sprt, vector<Texture> &txtr, vector<Sprite> &shadow, Music &jazz, string &the_word, string &path);
+    void logicFunction(RenderWindow& window,vector<Sprite> &sprt, vector<Texture> &txtr, vector<Sprite> &shadow, Music &jazz, string &the_word, string &path, int num, ifstream &themeFile, string str, vector<string> &words);
 };
